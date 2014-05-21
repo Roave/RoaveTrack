ca94abc83e0bf957ac028cd68014a3233d47acb4
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-05-21 22:48:07 +0200

    early draft, general UI components

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'thin'
+gem 'sinatra', '~> 1.4.4', require: 'sinatra/base'
+gem 'sinatra-contrib'
+gem 'erubis', '~> 2.7.0'
+gem 'sass', '~> 3.3.0'
---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'bundler/setup'
+Bundler.require
+require 'digest/md5'
+
+class App < Sinatra::Base
+  register Sinatra::Reloader
+
+  set :server, :thin
+  set :bind, '0.0.0.0'
+  set :port, 3005
+  helpers do
+    def gravatar(email, size=180)
+      "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}&d=mm&r=g"
+    end
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/blog' do
+    erb :blog
+  end
+
+  get '/css/*.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    filename = params[:splat].first
+    scss filename.to_sym, :views => "#{settings.root}/assets/stylesheets"
+  end
+
+  run! if app_file == $0
+end
